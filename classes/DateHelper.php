758e047bd2679f4645ebcb88205a70b1239eeaa5
Robin <robin.poulet@le-campus-numerique.fr>
2024-01-07 17:02:01 +0100

    #3 redécoupage classe WPContentManager + utilisation de try cacth + petit update de la page error

---
+++ classes/DateHelper.php
@@ -0,0 +1,24 @@
+<?php
+abstract class  HelperDate {
+    static public function getCurrentDateWeekDay() {
+        $date = new DateTime();
+        return (int) $date->format("N");
+    }
+    static public function getCurrentDatetime() {
+        $date = new DateTime();
+        return (int)$date->format("H") * 60 + (int)$date->format("i");
+    }
+    static public function dateDiff($date1_str, $date2_str) {
+        //var_dump($date1, $date2);
+        // Convertir les chaînes de date en objets DateTime
+        $date1 = new DateTime($date1_str);
+        $date2 = new DateTime($date2_str);
+        // Calculer la différence entre les deux dates
+        $interval = $date1->diff($date2);
+
+        // Obtenir le nombre de jours d'écart
+        $days_diff = $interval->days;
+        
+        return $days_diff;
+    }
+}
--- get-menu.php
+++ get-menu.php
@@ -1,23 +1,26 @@
 <?php
 require(__DIR__ . "/classes/WPContentManager.php");
+require(__DIR__ . "/classes/MenuManager.php");
 $cacheFile = './cache/menu.json';
 
 // Vérifier si le fichier de cache existe et est récent
-if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < 172800 /* 172800 48 heures */) {
+if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < 1 /* 86400 24 heures */) {
     // Charger les données à partir du cache
     $cachedData = file_get_contents($cacheFile);
     $postData = json_decode($cachedData, true);
 } else {
-    $currentDay = date("N"); 
-    // Vérifier si aujourd'hui est un dimanche ou un lundi
-    if ($currentDay == 1 || $currentDay == 7) {
-        $url = "http://www.nourriture-terrestre.fr";
-        $contextOptions = [
-            'http' => [
-                'method' => 'GET',
-                'header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-            ],
-        ];
+    // On essaye de reconstruire le cache
+    $url = "http://www.nourriture-terrestre.fr";
+    $contextOptions = [
+        'http' => [
+            'method' => 'GET',
+            'header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        ],
+    ];
+    $wpContent = new WPContentManager($url, $contextOptions);
+    try {
+        $dateMenu = $wpContent->getLastPostDate();
+        $lastPostLiElements = $wpContent->getLastPostLiElements();
         $menuHeader = [
             "entree",
             "plat 1",
@@ -25,33 +28,19 @@ if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < 172800 /* 1728
             "dessert 1",
             "dessert 2",
         ];
-        $wpContent = new WPContentManager($url, $contextOptions, $menuHeader);
-        $dateMenu = $wpContent->getLastArticleDate();
-        if ($dateMenu !== WPContentManager::ERROR_JSON && $dateMenu !== WPContentManager::ERROR_RESPONSE) {
-            $menu = $wpContent->displayLastArticleMenu();
-            if ($menu !== WPContentManager::MENU_NOT_AVAILABLE) {
-                $result = [
-                    "date" => $dateMenu,
-                    "menu" => $menu,
-                    "tata" => "tototot",
-                    "titi" => "tatata",
-                ];
-                // Sauvegarder les données dans le cache
-                file_put_contents($cacheFile, json_encode($result));
-                $postData = $result;
-            } else {
-                 // Rediriger vers la page d'erreur
-                 // TODO :: Créer une page d'erreur spécifique pour quand le menu n'est pas disponible
-                 header("Location: error.php");
-                 exit;
-            }
-        } else {
-            // Rediriger vers la page d'erreur
-            header("Location: error.php");
-            exit;
+        $menuManager = new MenuManager($lastPostLiElements, $menuHeader);
+        if ($menuManager->canDisplayMenu($dateMenu)) {
+            $menu = $menuManager->getMenuArray();
+            $result = [
+                "date" => $dateMenu,
+                "menu" => $menu,
+            ];
+             // Sauvegarder les données dans le cache
+             file_put_contents($cacheFile, json_encode($result));
+             $postData = $result;
         }
-    } else {
-        // Rediriger vers la page d'erreur de jour
+    } catch (\Exception $e) {
+        $_SESSION["error_message"] = $e->getMessage();
         header("Location: error.php");
         exit;
     }
