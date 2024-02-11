71215c28b6a5ff84a33be351d75d8288ebd15a2b
Robin <robin.poulet@le-campus-numerique.fr>
2024-02-11 15:01:44 +0100

    correction orhotgraphe

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function createDivAlert(data) {
         <div class="alert alert-success d-flex align-items-center" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
             <div>
-                Ta comande est bien enregirstrée ${user}
+                Ta commande est bien enregistrée ${user}
             </div>
         </div>
     `;
