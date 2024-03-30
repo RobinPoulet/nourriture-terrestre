24e3df3cc6acffed12ffb91a33bf449e2938a761
Robin <robin.poulet@le-campus-numerique.fr>
2024-03-30 13:09:37 +0100

    #19 suppression des try catch remplacés par des returnValue["error"]

--- classes/WPContentManager.php
+++ classes/WPContentManager.php
@@ -11,6 +11,11 @@ class WPContentManager {
         ],
     ];
     
+    /**
+    * @var string Paramètres de requêtes de l'API WordPress pour récupérer le dernier article
+    */
+    private string $lastPostRequestQuery = "/wp-json/wp/v2/posts?per_page=1&order=desc&orderby=date";
+    
      /**
      * Constructeur de la classe
      *
@@ -18,7 +23,7 @@ class WPContentManager {
      *
      * @return void
      */
-    public function __construct( // Utilisation nouvelle syntaxe simplifiée pour les constructeurs php 8
+    public function __construct(
         private string $url,
     ) {
     }
@@ -27,26 +32,28 @@ class WPContentManager {
     * Récupérer le dernier article WordPress
     *
     * @return array
-    * @throws Exception Renvoie une exception si pas de réponse de l'API ou problème lors de la conversion du JSON
     */
     private function getLastPost(): array 
     {
-        $requestQuery = "/wp-json/wp/v2/posts?per_page=1&order=desc&orderby=date";
-        $apiEndpoint = $this->url.$requestQuery;
+        $returnValue = [];
+        
+        $apiEndpoint = $this->url.$this->lastPostRequestQuery;
         $context = stream_context_create($this->contextOptions);
         $response = file_get_contents($apiEndpoint, false, $context);
 
         if ($response === false) {
-            throw new \Exception("Erreur dans la réponse de la requête.");
+            $returnValue["error"] = "Erreur dans la réponse de la requête.";
+        } else {
+            $postData = json_decode($response, true);
+
+            if (is_null($postData)) {
+                $returnValue["error"] = "Erreur lors de la conversion JSON.";
+            } else {
+                $returnValue["success"] = $postData;
+            }
         }
 
-        $postData = json_decode($response, true);
-
-        if (is_null($postData)) {
-            throw new \Exception("Erreur lors de la conversion JSON.");
-        }
-
-        return $postData;
+        return $returnValue;
     }
     
     /**
@@ -74,63 +81,69 @@ class WPContentManager {
     private function getLiElements(DOMDocument $doc): array 
     {
         $returnValue = [];
+        
         $liElements = $doc->getElementsByTagName('li');
-        foreach ($liElements as $li) {
-            $returnValue[] = $li->nodeValue;
-        }
+        $returnValue = array_map(function ($li) {
+            return $li->nodeValue;
+        }, iterator_to_array($liElements));
+        
         return $returnValue;
     }
     
     /**
     * Récupérer la date du dernier article WordPress
     *
-    * @return string
-    * @throws Exception Fais remonter l'exception de l'appel à l'API 
+    * @return array
     */
-    public function getLastPostDate(): string 
+    public function getLastPostDate(): array
     {
-        $returnValue = "";
-        try {
-            $lastArticle = $this->getLastPost();
-            $dateString = $lastArticle[0]['date'];
+        $returnValue = [];
+        
+        $lastPost = $this->getLastPost();
+        if (isset($lastPost["error"])) {
+            $returnValue["error"] = $lastPost["error"];
+        } elseif (!isset($lastPost[0]["date"])) {
+            $returnValue["error"] = "Pas de date dans cet article";
+        } else {
+            $dateString = $lastPost[0]['date'];
             // Convertir la chaîne de date en objet DateTime
             $dateObj = new DateTime($dateString);
             // Formater la date selon le format "Y-m-d"
             $returnValue = $dateObj->format("Y-m-d");
-            return $returnValue;
-        } catch (\Exception $e) {
-            throw $e;
         }
+           
+        return $returnValue;
     }
     
     /**
     * Récupérer les éléments de type <li></li> de l'article
     *
     * @return array
-    * @throws Exception Fais remonter les exceptions aux méthodes qui vont utiliser cette méthode
     */
 
     public function getLastPostLiElements() {
         $returnValue = [];
-        try {
-            $lastPost = $this->getLastPost();
+        
+        $lastPost = $this->getLastPost();
+        if (isset($lastPost["error"])) {
+            $returnValue["error"] = $lastPost["error"];
+        } elseif (isset($lastPost[0]["content"]["rendered"])) {
+            $returnValue["error"] = "Pas de contenu dans cet article";
+        } else {
             $doc = new DOMDocument();
             // Setup du loadHTML, pour utiliser les méthodes getElementsByName sans warning ni erreur
             $doc->loadHTML(
                 '<?xml encoding="UTF-8"><div>' . $lastPost[0]['content']['rendered'] . '</div>', 
                 LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOERROR | LIBXML_NOWARNING
             );
-            $returnValue = $this->getLiElements($doc);
-            // Si il n'y a pas de <li> dans l'article de la semaine, c'est une semaine sans menu on léve une exception
-            if (empty($returnValue)) {
+            $returnValue["success"] = $this->getLiElements($doc);
+            // Si il n'y a pas de <li> dans l'article de la semaine, c'est une semaine sans menu on envoie la page erreur spéciale pas de nourriture terrestre
+            if (empty($returnValue["success"])) {
                Header("Location: error-critic.html");
                die;
             }
-            
-            return $returnValue;
-        } catch (\Exception $e) {
-             // Lancez à nouveau l'exception pour propager l'erreur
-             throw $e;
         }
+        
+        return $returnValue;
     }
 }
