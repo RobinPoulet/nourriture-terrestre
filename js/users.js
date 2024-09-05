590ab3c8ee01514364190f8b2d0a8129669f9ec3
RobinPoulet <robin.poulet@le-campus-numerique.fr>
2024-09-05 07:25:14 +0200

    Commande : Faire une redirection vers "display-orders" après avoir commander #33 + petit refacto JS + correction bug sur edit comande

--- js/users.js
+++ js/users.js
@@ -2,17 +2,6 @@ $(document).ready(function () {
     const pathname = window.location.pathname;
     const pageName = pathname.split('/').pop();
     if (pageName === 'users.php') {
-        // Vérifier s'il y a un message stocké dans le sessionStorage
-        const toastMessage = sessionStorage.getItem('toastMessage');
-
-        if (toastMessage) {
-            // Afficher le toast avec le message stocké
-            displayToast(toastMessage, 'liveToast', 'liveToastContent');
-
-            // Supprimer le message du sessionStorage après affichage
-            sessionStorage.removeItem('toastMessage');
-        }
-
         // Sélectionner les éléments nécessaires
         const addUserModal = document.getElementById('addUserModal');
         const userNameInput = document.getElementById('userName');
@@ -77,7 +66,7 @@ function addUser() {
         url: 'ajax.php',
         data: data,
         success: function (response) {
-            ajaxSuccess(response);
+            ajaxSuccess(response, 'users.php');
         },
         error: function(xhr, status, error) {
             ajaxError(error);
@@ -112,7 +101,7 @@ function editUser(userId) {
         url: 'ajax.php',
         data: data,
         success: function (response) {
-            ajaxSuccess(response);
+            ajaxSuccess(response, 'users.php');
         },
         error: function(xhr, status, error) {
             ajaxError(error);
@@ -146,34 +135,10 @@ function deleteUser(userId) {
         url: 'ajax.php',
         data: data,
         success: function (response) {
-            ajaxSuccess(response);
+            ajaxSuccess(response, 'users.php');
         },
         error: function(xhr, status, error) {
             ajaxError(error);
         }
     });
-}
-
-function ajaxSuccess(response) {
-    // Code pour traiter la réponse AJAX
-    const data = JSON.parse(response);
-    if (data.success) {
-        // Stocker un message de succès dans le sessionStorage
-        sessionStorage.setItem('toastMessage', data.success);
-        // Déterminer l'URL de redirection en fonction de l'environnement
-        let baseUrl = window.location.origin;
-        if (baseUrl.includes('localhost')) {
-            baseUrl += '/nourriture-terrestre';
-        }
-        // Rediriger vers l'URL calculée
-        window.location.href = baseUrl + '/users.php';
-    } else {
-        displayToast(data.error.message, 'liveToast', 'liveToastContent');
-    }
-}
-
-function ajaxError(error) {
-    // Afficher un message d'erreur générique en cas d'erreur de requête AJAX
-    const toastMessage = "Une erreur s'est produite lors de la requête AJAX : " + error;
-    displayToast(toastMessage, 'liveToast', 'liveToastContent');
 }
