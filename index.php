a846e64468a09bd3282d44c8ab5d6fb513f80022
Robin <pouletrobin4@gmail.com>
2025-05-03 15:09:50 +0200

    Affichage des erreurs du mode dev ou mode debug et gestion des exceptions

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . "/vendor/autoload.php";
 
+use App\Core\ErrorHandler;
 use App\Core\Router;
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-// Initialiser le Router et gérer la requête
-$router = new Router();
-$router->handleRequest();
+// Détection de l'environnement
+$isDebugUrl = (isset($_GET["debug"]) && $_GET["debug"] === "1");
+$isDev = ($_SERVER["SERVER_NAME"] === "localhost" || $_SERVER["SERVER_ADDR"] === "127.0.0.1");
+$isDebug = ($isDev || $isDebugUrl);
+
+if ($isDebug) {
+    ini_set("display_errors", 1);
+    ini_set("display_startup_errors", 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set("display_errors", 0);
+    ini_set("display_startup_errors", 0);
+    error_reporting(0);
+}
+
+$handler = new ErrorHandler($isDebug);
+
+try {
+    $router = new Router();
+    $router->handleRequest();
+} catch (\Throwable $e) {
+    $handler->handle($e);
+}
---
+++ src/Core/ErrorHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Core;
+
+use Throwable;
+
+class ErrorHandler
+{
+    protected bool $debug;
+
+    public function __construct(bool $debug = false)
+    {
+        $this->debug = $debug;
+    }
+
+    /**
+     * @param Throwable $e
+     * @return void
+     */
+    public function handle(Throwable $e): void
+    {
+        error_log($e->getMessage() . "\n" . $e->getTraceAsString(), 3, BASE_PATH . "/logs/error.log");
+        http_response_code(500);
+        if ($this->debug) {
+            echo "<h1>Une erreur est survenue :</h1>";
+            echo "<p><strong>" . get_class($e) . ":</strong> " . $e->getMessage() . "</p>";
+            echo "<pre>" . $e->getTraceAsString() . "</pre>";
+        } else {
+            // Page d'erreur personnalisée
+            require_once BASE_PATH . "/views/errors/500.php";
+        }
+    }
+}
